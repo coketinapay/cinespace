aedc2b757508c36134adceb167871dec3f021aa8
coketinapay <tinapaycoke@gmail.com>
2025-07-29 19:20:43 +0800

    fixed variable

--- src/lib/api-shows-wrapper.ts
+++ src/lib/api-shows-wrapper.ts
@@ -7,7 +7,7 @@ export type ApiShowsWrapper = {
 export const apiShowsWrapper = ({ baseUrl, next }: ApiShowsWrapper) => {
   const apiCall = fetch(baseUrl, {
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTFlODE5Y2Q2OTg4NzI4YjU3YzgxYTdiNGQ2ODk0MiIsIm5iZiI6MTc1MTYxMTYxOS43MDEsInN1YiI6IjY4Njc3OGUzNGE2MzA5MTgyMTliYzc5NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FpDrqyb8_r2YUOBecAeeLWRRBL_vASpNVZXeUSsANEA`,
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_THEMOVIEDB_API_KEY || process.env.NEXT_PUBLIC_THEMOVIEDB_API_KEY}`,
     },
     next,
   });
